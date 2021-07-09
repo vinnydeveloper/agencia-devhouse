c6405fb4c92ee11c52df4f1e414d0e83aae4cbec
Vinicius Oliveira <vinicius.developer@outlook.com>
2021-07-08 22:02:55 -0300

    Adiconando pagina do blog

--- app.js
+++ app.js
@@ -2,12 +2,25 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-  res.send("Seja bem vindo");
+  res.sendFile(__dirname + "/views/home.html");
+});
+
+app.get("/home", (req, res) => {
+  res.redirect("/");
 });
 
 app.get("/manutencao", (req, res) => {
   res.sendFile(__dirname + "/views/manutencao.html");
 });
 
+app.get("/blog", (req, res) => {
+  res.sendFile(__dirname + "/views/blog.html");
+});
+
 app.listen(3000);
+
+// tem criar o arquivo home em HTML que contenha um titulo e um paragrafo
+// esse arquivo vai ser exibido tanto na rota / quanto na rota /home
